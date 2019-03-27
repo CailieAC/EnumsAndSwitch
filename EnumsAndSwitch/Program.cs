1bd1b6f75871db73a596be2e167cd1092ee13e21
CailieAC <cailiecarlile@gmail.com>
2019-03-26 22:49:50 -0500

    commit1

--- EnumsAndSwitch/Program.cs
+++ EnumsAndSwitch/Program.cs
@@ -28,6 +28,8 @@ namespace EnumsAndSwitch
                 new Todo { Description = "Task 14", EstimatedHours = 6, Status = Status.Completed }
             };
 
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+
             PrintAssessment(todos);
             Console.ReadLine();
 
@@ -37,7 +39,41 @@ namespace EnumsAndSwitch
         {
             foreach (var todo in todos)
             {
+                //switch tab tab
+                /*switch (todo.Status)
+                {
+                    case Status.Completed:
+                        //input code
+                        break;
+                    case Status.Deleted:
+                        //input code
+                        break;
+                    default:
+                        break;
+                }*/
 
+                //type 'switch (todo.Status); and hit enter twice for it to autofill each status case
+                switch (todo.Status)
+                {
+                    case Status.NotStarted:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        break;
+                    case Status.InProgress:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        break;
+                    case Status.OnHold:
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        break;
+                    case Status.Completed:
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        break;
+                    case Status.Deleted:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        break;
+                    default:
+                        break;
+                }
+                Console.WriteLine(todo.Description);
             }
         }
     }
@@ -51,6 +87,7 @@ namespace EnumsAndSwitch
 
     enum Status
     {
+        //hover over to see numeric value stored
         NotStarted,
         InProgress,
         OnHold,
